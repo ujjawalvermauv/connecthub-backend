dde5949a59f103037088a1aeab643e49298a598f
ujjawal <ujjawalvermauv12@gmail.com>
2026-04-26 12:02:42 +0530

    Update assembly informational version and cache files for ConnectHub.Auth and ConnectHub.Message

--- ConnectHub.Auth/obj/Debug/net10.0/ConnectHub.Auth.AssemblyInfo.cs
+++ ConnectHub.Auth/obj/Debug/net10.0/ConnectHub.Auth.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectHub.Auth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bef394258c172a7b37badabb20c1f8e5c7b0b6d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fe31e8913b4c4431b1d3609d6a4076e46fc196e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectHub.Auth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectHub.Auth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
